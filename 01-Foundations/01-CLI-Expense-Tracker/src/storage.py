49f1ed34825e43aafd5d8572659935dc8aa6906d
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-13 13:06:45 +0500

    Progress Started

---
+++ 01-Foundations/01-CLI-Expense-Tracker/src/storage.py
@@ -0,0 +1,13 @@
+import json 
+
+DATA_FILE = "data/transactions.json"
+
+def load_transactions():
+    with open(DATA_FILE, "r") as file:
+        transactions = json.load(file)
+
+    return transactions
+
+def save_transcations(transcations):
+    with open(DATA_FILE, "w") as file:
+        json.dump(transcations, file, indent=4)
---
+++ 01-Foundations/01-CLI-Expense-Tracker/src/transactions.py
@@ -0,0 +1,12 @@
+def add_transaction(transactions):
+    print("Add Transaction!")
+
+    transaction_type = input("Enter type (income, expense): ")
+    amount = float(input("Enter Amount: "))
+    category = input("Enter Category: ")
+    description = input("Enter Description: ")
+
+
+
+transactions = []
+add_transaction(transactions)
