73cf47ecbc306841c9c7093fdf416a6c115d5ed8
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-14 10:42:39 +0500

    Update transactions.py

--- 01-Foundations/01-CLI-Expense-Tracker/src/transactions.py
+++ 01-Foundations/01-CLI-Expense-Tracker/src/transactions.py
@@ -6,6 +6,14 @@ def add_transaction(transactions):
     category = input("Enter Category: ")
     description = input("Enter Description: ")
 
+    transactions = {
+
+    "type"= transaction_type,
+    "amount" = amount,
+    "category" = category,
+    "description" = description
+    }
+
 
 
 transactions = []
