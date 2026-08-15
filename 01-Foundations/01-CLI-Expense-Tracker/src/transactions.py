126ab0e6e65513a1d2f403cd6158a863b539894e
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-15 12:56:10 +0500

    Added user validation in add_trasaction function

--- 01-Foundations/01-CLI-Expense-Tracker/src/transactions.py
+++ 01-Foundations/01-CLI-Expense-Tracker/src/transactions.py
@@ -1,15 +1,47 @@
-from storage import save_transactions
+from storage import save_transactions, load_transactions
 
 def add_transaction(transactions):
     print("Add Transaction!")
 
-    transaction_type = input("Enter type (income, expense): ")
-    amount = float(input("Enter Amount: "))
-    category = input("Enter Category: ")
-    description = input("Enter Description: ")
+    while True:
+        transaction_type = input("Enter type (income/exepnse): ").lower()
+
+        if transaction_type in ['income', 'expense']:
+            break
+
+        print("Invalid type. Please enter income or expense.")
+
+    while True:
+        try: 
+            amount = float(input("Enter amount: "))
+
+            if amount > 0:
+                break 
+
+            print("Amount must be greater than 0. ")
+        except ValueError:
+            print("Invalid Amount. Please enter a number.")
+
+    while True:
+        category = input("Enter Category: ").strip()
+
+        if category:
+            break 
+
+        print("Category cannot be empty.")
+
+    while True:
+        description = input("Enter Description: ").strip()
+
+        if description:
+            break 
+
+        print("Description cannot be empty.")
+    
+    transaction_id = len(transactions) + 1 
 
     transaction = {
-
+    "id": transaction_id,
     "type": transaction_type,
     "amount":  amount,
     "category": category,
@@ -18,8 +50,9 @@ def add_transaction(transactions):
     
     transactions.append(transaction)
     save_transactions(transactions)
+    print(transactions)
 
 
 
-transactions = []
+transactions = load_transactions()
 add_transaction(transactions)
