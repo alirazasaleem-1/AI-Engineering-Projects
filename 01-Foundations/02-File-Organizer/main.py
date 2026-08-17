15011165123942024a897d3ab2f7724796fa8972
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-17 12:34:02 +0500

    Classifying by extension

--- 01-Foundations/02-File-Organizer/main.py
+++ 01-Foundations/02-File-Organizer/main.py
@@ -1,6 +1,14 @@
 from pathlib import Path 
 
-folder = Path(r"D:\PythonProjects\AI-Engineering-Projects\01-Foundations\02-File-Organizer")
+test_folder = Path(r"D:\PythonProjects\AI-Engineering-Projects\01-Foundations\02-File-Organizer")
 
-for item in folder.rglob("*"):
-    print(item)
+for item in test_folder.iterdir():
+    if item.is_file():
+        print(f"File Name: {item.name}")
+        print(f"Extension: {item.suffix}")
+        if item.suffix in ['.jpg', '.png']:
+            print("It's an image")
+            category = "image"
+        if item.suffix in ['.pdf', '.doc', '.docx']:
+            print("It's a document.")
+            category = "document"
