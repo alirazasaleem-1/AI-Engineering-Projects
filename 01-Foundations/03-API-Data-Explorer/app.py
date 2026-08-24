09755e8e9e4ba7e6fdb3979ca978cd0e37150098
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-24 12:07:56 +0500

    Added timeout

--- 01-Foundations/03-API-Data-Explorer/app.py
+++ 01-Foundations/03-API-Data-Explorer/app.py
@@ -5,13 +5,13 @@ base_url = "https://pokeapi.co/api/v2"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
 
     if response.status_code == 200:
         pokemon_data = response.json()
         return pokemon_data
     else:
-        print("Failed to retrieve data {response.status_code}")
+        print(f"Failed to retrieve data {response.status_code}")
 
 pokemon_name = "pikachu"
 pokemon_info = get_pokemon_info(pokemon_name)
