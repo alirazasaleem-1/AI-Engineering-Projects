03efe47c2cb6cbd334176121bee83fc17cddb308
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-25 11:41:32 +0500

    Separated API Client from the app.py

--- 01-Foundations/03-API-Data-Explorer/app.py
+++ 01-Foundations/03-API-Data-Explorer/app.py
@@ -1,30 +1,5 @@
+from src.api_client import get_pokemon_info
 import requests
-
-base_url = "https://pokeapi.co/api/v2"
-
-def get_pokemon_info(name):
-    url = f"{base_url}/pokemon/{name}"
-
-    response = requests.get(url, timeout=10)
-
-    if response.status_code == 200:
-        pokemon_data = response.json()
-
-        required_fields = ['name', 'id', 'height', 'weight']
-
-        if all(field in pokemon_data for field in required_fields):
-            return pokemon_data
-        else:
-            print("Invalid API response")
-
-    elif response.status_code == 404:
-        print(f"Pokemon not Found. Error: {response.status_code}")
-
-    elif response.status_code >= 500:
-        print(f"PokeAPI server error. Try again later. Error: {response.status_code}")
-
-    else:
-        print(f"Failed to retrieve data {response.status_code}")
 
 pokemon_name = input("Enter Pokemon name: ").strip().lower()
 pokemon_info = get_pokemon_info(pokemon_name)
