333e58295275a18877c21de9b032b00d09c8fba4
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-28 11:19:59 +0500

    ran a test

--- 01-Foundations/03-API-Data-Explorer/tests/test_api_client.py
+++ 01-Foundations/03-API-Data-Explorer/tests/test_api_client.py
@@ -6,3 +6,9 @@ def test_get_pokemon_info():
     assert pokemon is not None 
     assert pokemon.name == 'pikachu'
     assert pokemon.id == 25
+
+def test_invalid_pokemon():
+    pokemon = get_pokemon_info("this-pokemon-does-not-exist")
+
+    assert pokemon is None
+
