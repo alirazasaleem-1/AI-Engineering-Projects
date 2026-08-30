a1364c9822f826191aba2fd0c81f8b5fa5890dac
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-08-31 00:44:45 +0500

    Finished basic v1 of ai text summarizer

--- 02-Intermediate/01-AI-Text-Summarizer/main.py
+++ 02-Intermediate/01-AI-Text-Summarizer/main.py
@@ -1,4 +1,29 @@
-from google import genai
-from dotenv import load_dotenv 
-import os 
+# AI TEXT SUMMARIZER 
 
+# Imports 
+import os 
+from dotenv import load_dotenv
+from google import genai
+from pathlib import Path 
+
+# Get the API to create gemini client
+env_path = Path(__file__).parent / ".env"
+load_dotenv(env_path)
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+# Summarize Function using our client
+def summarize(text):
+    response = client.models.generate_content(
+        model="gemini-3.6-flash",
+        contents=f"Summarize this text and provide summarized, brief, consize simple text without stars etc in response. Just simple summarized text.\n\n{text}"
+    )
+
+    return response.text 
+
+# Input and Output
+print("==== ALI'S AI TEXT SUMMARIZER ====")
+text = input("Enter Text to Summarize: \n")
+summary = summarize(text)
+print("==== SUMMARY ====")
+print(summary)
