99eeab9b522cdaf4908b07149a61abdcd6e5a902
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-09-15 11:54:56 +0500

    Used sentiment analysis model and display results beautifully

--- 02-Intermediate/03-HuggingFace-NLP-Toolkit/nlp_toolkit.py
+++ 02-Intermediate/03-HuggingFace-NLP-Toolkit/nlp_toolkit.py
@@ -41,5 +41,15 @@ if st.button("Analyze"):
             model = pipeline("question-answering")
         elif task == "Named Entity Recognition":
             model = pipeline("ner")
+
+        st.session_state.loaded_model = model 
+        result = model(text)
+        st.subheader("📊 Results")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric("Sentiment", f"result[0]['label']")
+        with col2:
+            st.metric("Confidence:", f"{result[0]['score']:.2%}")
     else:
         st.warning("⚠ Please enter text first!")
