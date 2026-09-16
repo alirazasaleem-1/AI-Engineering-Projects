1b6d9345549cc52f6c215cdc5645049dfaded26d
Ali Raza Saleem <alirazasaleem099@gmail.com>
2026-09-16 11:49:23 +0500

    Added loading spineer

--- 02-Intermediate/03-HuggingFace-NLP-Toolkit/nlp_toolkit.py
+++ 02-Intermediate/03-HuggingFace-NLP-Toolkit/nlp_toolkit.py
@@ -33,34 +33,35 @@ text = st.text_area("Paste your text here:", height=100)
 # Submit button
 if st.button("Analyze"):
     if text:
-        st.write("Processing...")
+        with st.spinner("🔃 Loading Model..."):
+            st.write("Processing...")
 
-        if task != st.session_state.current_task:
-            st.session_state.loaded_model = None
-            st.session_state.current_task = task 
+            if task != st.session_state.current_task:
+                st.session_state.loaded_model = None
+                st.session_state.current_task = task 
 
-        if task == "Sentiment Analysis":
-            if st.session_state.loaded_model is None:
-                st.session_state.loaded_model = pipeline("sentiment-analysis")
-        elif task == "Text Classification":
-            if st.session_state.loaded_model is None:
-                st.session_state.loaded_model = pipeline("zero-shot-classification")
-        elif task == "Named Entity Recognition":
-            if st.session_state.loaded_model is None:
-                st.session_state.loaded_model = pipeline("ner")
+            if task == "Sentiment Analysis":
+                if st.session_state.loaded_model is None:
+                    st.session_state.loaded_model = pipeline("sentiment-analysis")
+            elif task == "Text Classification":
+                if st.session_state.loaded_model is None:
+                    st.session_state.loaded_model = pipeline("zero-shot-classification")
+            elif task == "Named Entity Recognition":
+                if st.session_state.loaded_model is None:
+                    st.session_state.loaded_model = pipeline("ner")
 
-        model = st.session_state.loaded_model 
-        try:
-            result = model(text)
-            st.subheader("📊 Results")
-            
-            col1, col2 = st.columns(2)
-            with col1:
-                st.metric("Sentiment", result[0]['label'])
-            with col2:
-                st.metric("Confidence:", f"{result[0]['score']:.2%}")
-        except Exception as e:
-            st.error("❌ Error! Could not analyze. Try again.")
+            model = st.session_state.loaded_model 
+            try:
+                result = model(text)
+                st.subheader("📊 Results")
+                
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.metric("Sentiment", result[0]['label'])
+                with col2:
+                    st.metric("Confidence:", f"{result[0]['score']:.2%}")
+            except Exception as e:
+                st.error("❌ Error! Could not analyze. Try again.")
         
     else:
         st.warning("⚠ Please enter text first!")
